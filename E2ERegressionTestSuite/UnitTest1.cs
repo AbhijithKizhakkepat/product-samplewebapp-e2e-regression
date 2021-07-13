e9a551dcf02d13fdee523fa13ee7af1b49ff697a
AbhijithKizhakkepat <AbhijithKizhakkepat@users.noreply.github.com>
2021-07-13 14:43:47 +1000

    Initial Commit - New Unit Test Project added to a new blank Solution. Installed necessary NuGet packages to be able to test web application using BDD approach.

---
+++ E2ERegressionTestSuite/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace E2ERegressionTestSuite
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
